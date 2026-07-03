4e1fe9a36f99427e2b51904bea46397ce088e214
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-03 20:37:48 +0100

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,28 @@
+import sqlite3
+from pathlib import Path
 
+
+class Database:
+
+    def __init__(self):
+
+        self.db_path = Path("database/athena.db")
+
+        self.schema_path = Path("database/schema.sql")
+
+
+    def initialize(self):
+
+        connection = sqlite3.connect(self.db_path)
+
+        cursor = connection.cursor()
+
+        with open(self.schema_path, "r") as file:
+
+            cursor.executescript(file.read())
+
+        connection.commit()
+
+        connection.close()
+
+        print("✓ Database initialized.")
