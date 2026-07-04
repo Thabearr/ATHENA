b7784d47a62791a28f0eb14f9a8dee3f8d6fe766
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-04 11:52:50 +0100

    Update client.py

--- api/client.py
+++ api/client.py
@@ -1,1 +1,16 @@
+import httpx
 
+
+class APIClient:
+    def __init__(self, api_key: str):
+        self.client = httpx.Client(
+            headers={
+                "x-apisports-key": api_key
+            },
+            timeout=20
+        )
+
+    def get(self, url, params=None):
+        response = self.client.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
