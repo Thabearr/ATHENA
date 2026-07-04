47d5ff4ddcacd60e394907e3a8599e1cf6a5d386
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-04 12:07:03 +0100

    Update fixture.py

--- models/fixture.py
+++ models/fixture.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass
+from datetime import datetime
 
+
+@dataclass
+class Fixture:
+    fixture_id: int
+    league: str
+    league_id: int
+
+    home_team: str
+    away_team: str
+
+    home_team_id: int
+    away_team_id: int
+
+    kickoff: datetime
+
+    venue: str
+
+    status: str
