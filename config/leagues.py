a2bab0ff456ef34373ade79ca5dc7a54000a6ac0
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-04 12:08:27 +0100

    Update leagues.py

--- config/leagues.py
+++ config/leagues.py
@@ -1,1 +1,13 @@
-
+TOP_LEAGUES = {
+    39: "Premier League",
+    140: "La Liga",
+    135: "Serie A",
+    78: "Bundesliga",
+    61: "Ligue 1",
+    88: "Eredivisie",
+    203: "Turkish Super Lig",
+    144: "Belgian Pro League",
+    207: "Swiss Super League",
+    103: "Eliteserien",
+    113: "Allsvenskan"
+}
