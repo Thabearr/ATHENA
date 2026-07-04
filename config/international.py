53a5556421407b8c0fe0dae04f522c10c6aadb8c
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-04 12:09:25 +0100

    Update international.py

--- config/international.py
+++ config/international.py
@@ -1,1 +1,8 @@
-
+INTERNATIONAL_COMPETITIONS = {
+    1: "World Cup",
+    4: "Euro Championship",
+    5: "UEFA Nations League",
+    6: "Africa Cup of Nations",
+    7: "World Cup Qualification",
+    8: "Euro Qualification"
+}
