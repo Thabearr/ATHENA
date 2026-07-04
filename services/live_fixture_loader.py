3508aa3634eaf5ec89c3df0add402f06deb82b12
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-04 12:15:52 +0100

    Update live_fixture_loader.py

--- services/live_fixture_loader.py
+++ services/live_fixture_loader.py
@@ -1,1 +1,16 @@
+from api.football import FootballProvider
+from config.settings import settings
+from services.fixture_service import FixtureService
 
+
+class LiveFixtureLoader:
+
+    def __init__(self):
+
+        provider = FootballProvider(settings.FOOTBALL_API_KEY)
+
+        self.service = FixtureService(provider)
+
+    def load(self):
+
+        return self.service.get_today_fixtures()
