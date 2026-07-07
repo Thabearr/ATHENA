6474bcdc6ed980a85a33aa37f537f7d1ff6b2eef
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-08 00:22:50 +0100

    Create markets.py

---
+++ models/markets.py
@@ -0,0 +1,109 @@
+MARKETS = {
+
+    "TO_QUALIFY": [
+        "Home",
+        "Away"
+    ],
+
+    "ASIAN_HANDICAP": [
+        "Home -0.5",
+        "Away +0.5",
+
+        "Home -1.0",
+        "Away +1.0",
+
+        "Home -1.5",
+        "Away +1.5",
+
+        "Home -2.0",
+        "Away +2.0",
+
+        "Home -2.5",
+        "Away +2.5",
+
+        "Home -3.0",
+        "Away +3.0",
+
+        "Home -3.5",
+        "Away +3.5"
+    ],
+
+    "OVER_UNDER": [
+        "Over 0.5",
+        "Over 1.5",
+        "Over 2.5",
+        "Over 3.5",
+        "Over 4.5",
+        "Over 5.5",
+
+        "Under 0.5",
+        "Under 1.5",
+        "Under 2.5",
+        "Under 3.5",
+        "Under 4.5",
+        "Under 5.5"
+    ],
+
+    "DOUBLE_CHANCE": [
+        "Home or Draw",
+        "Home or Away",
+        "Draw or Away"
+    ],
+
+    "DRAW_OR_OVER_2_5": [
+        "Yes",
+        "No"
+    ],
+
+    "AWAY_OR_OVER_2_5": [
+        "Yes",
+        "No"
+    ],
+
+    "HOME_OR_OVER_2_5": [
+        "Yes",
+        "No"
+    ],
+
+    "HOME_WIN_EITHER_HALF": [
+        "Yes",
+        "No"
+    ],
+
+    "AWAY_WIN_EITHER_HALF": [
+        "Yes",
+        "No"
+    ],
+
+    "GG_NG": [
+        "Yes",
+        "No"
+    ],
+
+    "HOME_WIN_TO_NIL": [
+        "Yes",
+        "No"
+    ],
+
+    "AWAY_WIN_TO_NIL": [
+        "Yes",
+        "No"
+    ],
+
+    "DRAW_NO_BET": [
+        "Home",
+        "Away"
+    ],
+
+    "ONE_X_TWO_1UP": [
+        "Home",
+        "Draw",
+        "Away"
+    ],
+
+    "ONE_X_TWO_2UP": [
+        "Home",
+        "Draw",
+        "Away"
+    ]
+}
