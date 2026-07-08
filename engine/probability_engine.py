c4db08722b8f212a9df0677feee9b8d0ab435a79
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-08 02:11:39 +0100

    Update probability_engine.py

--- engine/probability_engine.py
+++ engine/probability_engine.py
@@ -1,1 +1,33 @@
+from models.prediction import Prediction
 
+
+class ProbabilityEngine:
+
+    def calculate(self, prediction: Prediction):
+
+        total_strength = (
+            prediction.home_strength +
+            prediction.away_strength
+        )
+
+        if total_strength == 0:
+            return prediction
+
+        prediction.home_win_probability = (
+            prediction.home_strength /
+            total_strength
+        ) * 100
+
+        prediction.away_win_probability = (
+            prediction.away_strength /
+            total_strength
+        ) * 100
+
+        prediction.draw_probability = max(
+            0,
+            100 -
+            prediction.home_win_probability -
+            prediction.away_win_probability
+        )
+
+        return prediction
