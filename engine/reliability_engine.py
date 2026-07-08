463eb652948a9fa8a6f2af16612d5ba59cc19818
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-08 02:13:38 +0100

    Update reliability_engine.py

--- engine/reliability_engine.py
+++ engine/reliability_engine.py
@@ -1,1 +1,21 @@
+from models.prediction import Prediction
 
+
+class ReliabilityEngine:
+
+    def evaluate(self, prediction: Prediction):
+
+        confidence = 50
+
+        difference = abs(
+            prediction.home_strength -
+            prediction.away_strength
+        )
+
+        confidence += difference
+
+        confidence -= prediction.risk_score / 2
+
+        prediction.confidence = min(confidence, 99)
+
+        return prediction
