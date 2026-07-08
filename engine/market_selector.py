cc0014e90d190019199776d7e6106721a52b71af
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-08 02:15:36 +0100

    Update market_selector.py

--- engine/market_selector.py
+++ engine/market_selector.py
@@ -1,1 +1,33 @@
+from models.prediction import Prediction
 
+
+class MarketSelector:
+
+    def select(self, prediction: Prediction):
+
+        # Default recommendation
+        prediction.recommended_market = "No Recommendation"
+
+        # Very high confidence
+        if prediction.confidence >= 90:
+
+            if prediction.home_win_probability > 70:
+                prediction.recommended_market = "Home -0.5"
+
+            elif prediction.away_win_probability > 70:
+                prediction.recommended_market = "Away +0.5"
+
+            else:
+                prediction.recommended_market = "Home or Draw"
+
+        # Medium confidence
+        elif prediction.confidence >= 75:
+
+            prediction.recommended_market = "Home or Draw"
+
+        # Low confidence
+        else:
+
+            prediction.recommended_market = "No Recommendation"
+
+        return prediction
