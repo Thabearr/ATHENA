c5b28c7b35d11098e5213a2b1feb59bcd5319680
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-11 16:23:27 +0100

    Update team_repository.py

--- repositories/team_repository.py
+++ repositories/team_repository.py
@@ -4,88 +4,46 @@ from database.database import Database
 class TeamRepository:
 
     def __init__(self):
+
         self.db = Database()
 
-    def get_team(self, team_id):
-
-        query = """
-            SELECT *
-            FROM teams
-            WHERE team_id = ?
-        """
-
-        with self.db.connect() as conn:
-
-            conn.row_factory = lambda cursor, row: {
-                cursor.description[i][0]: row[i]
-                for i in range(len(row))
-            }
-
-            cursor = conn.cursor()
-
-            cursor.execute(query, (team_id,))
-
-            return cursor.fetchone()
-
-    def save_team(
+    def update_form(
         self,
         team_id,
-        name,
-        country,
-        league
+        form,
+        wins,
+        draws,
+        losses,
+        goals_for,
+        goals_against,
     ):
-
-        query = """
-        INSERT OR REPLACE INTO teams
-        (
-            team_id,
-            name,
-            country,
-            league
-        )
-        VALUES
-        (
-            ?,?,?,?
-        )
-        """
 
         with self.db.connect() as conn:
 
-            cursor = conn.cursor()
+            conn.execute(
+                """
+                UPDATE teams
 
-            cursor.execute(
-                query,
+                SET
+                    form=?,
+                    recent_wins=?,
+                    recent_draws=?,
+                    recent_losses=?,
+                    recent_goals_for=?,
+                    recent_goals_against=?
+
+                WHERE team_id=?
+                """,
+
                 (
+                    form,
+                    wins,
+                    draws,
+                    losses,
+                    goals_for,
+                    goals_against,
                     team_id,
-                    name,
-                    country,
-                    league
-                )
+                ),
             )
 
             conn.commit()
-
-    def team_exists(self, team_id):
-
-        return self.get_team(team_id) is not None
-
-    def get_all_teams(self):
-
-        query = """
-            SELECT *
-            FROM teams
-            ORDER BY name
-        """
-
-        with self.db.connect() as conn:
-
-            conn.row_factory = lambda cursor, row: {
-                cursor.description[i][0]: row[i]
-                for i in range(len(row))
-            }
-
-            cursor = conn.cursor()
-
-            cursor.execute(query)
-
-            return cursor.fetchall()
