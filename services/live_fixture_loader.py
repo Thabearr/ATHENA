9ddb52e4987b94baaf013a2ea14dd5cde728ebad
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-11 15:42:20 +0100

    Update live_fixture_loader.py

--- services/live_fixture_loader.py
+++ services/live_fixture_loader.py
@@ -1,5 +1,6 @@
 from api.football_api import FootballProvider
 from config.settings import settings
+from config.supported_leagues import SUPPORTED_LEAGUES
 from services.fixture_storage import FixtureStorage
 
 
@@ -13,6 +14,16 @@ class LiveFixtureLoader:
     def load_today(self):
 
         fixtures = self.provider.get_today_fixtures()
+
+        fixtures = [
+
+            fixture
+
+            for fixture in fixtures
+
+            if fixture["league"]["id"] in SUPPORTED_LEAGUES
+
+        ]
 
         self.storage.save(fixtures)
 
