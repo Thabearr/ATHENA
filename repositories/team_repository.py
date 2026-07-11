875aa9b8fa957ce2c5fcfa0525c790544d02ac30
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-11 16:27:35 +0100

    Update team_repository.py

--- repositories/team_repository.py
+++ repositories/team_repository.py
@@ -7,6 +7,74 @@ class TeamRepository:
 
         self.db = Database()
 
+    def save_team(
+        self,
+        team_id,
+        name,
+        country,
+        league_id,
+        season,
+    ):
+
+        with self.db.connect() as conn:
+
+            conn.execute(
+                """
+                INSERT INTO teams
+                (
+                    team_id,
+                    name,
+                    country,
+                    league_id,
+                    season
+                )
+
+                VALUES (?, ?, ?, ?, ?)
+
+                ON CONFLICT(team_id)
+                DO UPDATE SET
+
+                    name=excluded.name,
+                    country=excluded.country,
+                    league_id=excluded.league_id,
+                    season=excluded.season
+                """,
+
+                (
+                    team_id,
+                    name,
+                    country,
+                    league_id,
+                    season,
+                ),
+            )
+
+            conn.commit()
+
+    def get_team(self, team_id):
+
+        with self.db.connect() as conn:
+
+            conn.row_factory = lambda cursor, row: {
+                cursor.description[i][0]: row[i]
+                for i in range(len(row))
+            }
+
+            cur = conn.cursor()
+
+            cur.execute(
+
+                """
+                SELECT *
+                FROM teams
+                WHERE team_id=?
+                """,
+
+                (team_id,),
+            )
+
+            return cur.fetchone()
+
     def update_form(
         self,
         team_id,
@@ -25,11 +93,17 @@ class TeamRepository:
                 UPDATE teams
 
                 SET
+
                     form=?,
+
                     recent_wins=?,
+
                     recent_draws=?,
+
                     recent_losses=?,
+
                     recent_goals_for=?,
+
                     recent_goals_against=?
 
                 WHERE team_id=?
