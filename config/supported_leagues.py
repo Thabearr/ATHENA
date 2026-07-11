87c8989e9eec311cb5c8922573c009e23a99d2c6
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-11 15:41:25 +0100

    Create supported_leagues.py

---
+++ config/supported_leagues.py
@@ -0,0 +1,91 @@
+SUPPORTED_LEAGUES = {
+
+    # England
+    39,
+
+    # Spain
+    140,
+
+    # Italy
+    135,
+
+    # Germany
+    78,
+
+    # France
+    61,
+
+    # Netherlands
+    88,
+
+    # Portugal
+    94,
+
+    # Turkey
+    203,
+
+    # Belgium
+    144,
+
+    # Switzerland
+    207,
+
+    # Scotland
+    179,
+
+    # Norway
+    103,
+
+    # Sweden
+    113,
+
+    # Denmark
+    119,
+
+    # Austria
+    218,
+
+    # Greece
+    197,
+
+    # Czech Republic
+    345,
+
+    # Croatia
+    210,
+
+    # Serbia
+    286,
+
+    # Poland
+    106,
+
+    # Romania
+    283,
+
+    # UEFA Champions League
+    2,
+
+    # Europa League
+    3,
+
+    # Europa Conference League
+    848,
+
+    # World Cup
+
+    1,
+
+    # EURO
+
+    4,
+
+    # Nations League
+
+    5,
+
+    # AFCON
+
+    6
+
+}
