50ee38cbe4ff911e2a38b16a728a61729220a499
Moses Oluwasegun <113853913+Thabearr@users.noreply.github.com>
2026-07-16 05:48:58 +0100

    Create openfootball_mapping.py

---
+++ config/openfootball_mapping.py
@@ -0,0 +1,9 @@
+OPENFOOTBALL_LEAGUE_CODES = {
+    203: "tr.1",   # Turkey
+    144: "be.1",   # Belgium
+    179: "sco.1",  # Scotland
+    218: "at.1",   # Austria
+    197: "gr.1",   # Greece
+}
+
+OPENFOOTBALL_SEASON = "2025-26"
